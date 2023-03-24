794c9627729c2860dc50cd1c00d50452658a4dc6
GiuliaSetti <giulia.setti96@gmail.com>
2023-03-24 18:11:44 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script>
+
+  import AppNavbar from './components/AppNavbar.vue'
+  import AppMainHeader from './components/AppMainHeader.vue'
+  import AppMain from './components/AppMain.vue'
+  import AppFooterUp from './components/AppFooterUp.vue'
+  import AppFooter from './components/AppFooter.vue'
+
+  export default{
+    data(){
+      return{
+
+      }
+  },
+
+
+  components:{
+    AppNavbar,
+    AppMainHeader,
+    AppMain,
+    AppFooterUp,
+    AppFooter
+  }
+}
+
+</script>
+
+
+<template>
+
+  <div>
+    <AppNavbar></AppNavbar>
+  </div>
+
+ 
+
+  <div>
+    <AppMainHeader></AppMainHeader>
+    <AppMain></AppMain>
+  </div>
+
+  <div>
+    <AppFooterUp></AppFooterUp>
+    <AppFooter></AppFooter>
+  </div>
+
+
+
+</template>
+
+<style lang="scss">
+
+ 
+
+
+</style>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,91 @@
+<script>
+
+export default{
+    data(){
+      return{
+
+        links:[
+          "Character",
+          "Comics",
+          "Movies",
+          "tv",
+          "games",
+          "collectibles",
+          "videos",
+          "fans",
+          "news",
+          "shop"
+        ]
+
+
+
+      }
+  }
+}
+
+</script>
+
+<template>
+  <!-- header -->
+    <header>
+
+      <!-- navbar -->
+      <nav class="centered">
+
+
+        <!-- navleft -->
+        <div class="nav_left">
+          <img src="public/img/dc-logo.png" alt="">
+        </div>
+
+        <!-- navright -->
+        <div class="nav_right">
+          <ul>
+            <li v-for="link in links">{{link}}</li>
+          </ul>
+        </div>
+
+      </nav>
+    </header>
+
+</template>
+
+
+<style lang="scss">
+
+  nav{
+    padding: 0 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+  
+
+    img{
+      width: 60px;
+    }
+
+    
+    ul{
+      display: flex;
+
+      li{
+      padding: 25px 15px;
+      text-transform: uppercase;
+
+      color: gray;
+      font-weight: bold;
+      }
+
+
+      li:hover{
+        color: rgb(25, 116, 244);
+        border-bottom: 2px solid  rgb(25, 116, 244);
+      }
+
+    }
+  }
+
+
+
+</style>
